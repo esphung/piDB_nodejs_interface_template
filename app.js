dbb8d661b946d74fae2df298a99b8c0ae2f887df
Eric Phung <esphung@iupui.edu>
2016-03-05 03:58:50 +0000

    finally parsed JSON!

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
+
+
+
+
+
+
+var http = require('http');
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-const PORT = 8000;
+var jsonfile = require('jsonfile')
+var util = require('util')
+var fs = require('fs')
 
+
+
+
+
+
+// local vars
+const PORT = 8000;
 var app = express();
 
 
@@ -10,6 +26,12 @@ var app = express();
 // configure app
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname, 'views'));
+
+
+
+
+
+
 
 
 
@@ -24,8 +46,41 @@ app.use(express.static(path.join(__dirname,'bower_components')));
 
 
 
-// define routes
 
+
+
+console.log("\n *STARTING: READ DATABASE* \n");
+// Get content from file
+var contents = fs.readFileSync("people.json");
+// Define to JSON type
+var jsonContent = JSON.parse(contents);
+
+
+// Get Value from JSON
+console.log("DataBase Name: ", jsonContent.databaseName);
+console.log("DataBase Records: ",jsonContent.people);
+
+
+// get all last name properties
+jsonContent.people.forEach(function (item) {
+	console.log(item.lastName)
+})
+
+
+/*console.log("Email:", jsonContent.email);
+console.log("Password:", jsonContent.password);*/
+console.log("\n *EXIT: READ DATABASE* \n");
+
+
+//var currentItems = peopleDatabase.people.id;
+
+//console.log(currentItems)
+
+/*var people_json = jsonfile.readFile(file, function(err, obj) {
+  console.dir(obj);
+});*/
+
+//var people_json = jsonfile.readFileSync(file)
 
 
 
@@ -33,14 +88,27 @@ var currentItems = [
 	{ id: 1, desc: 'Eric' },
 	{ id: 2, desc: 'Cheryl' },
 	{ id: 3, desc: 'Avery' }
-];
+]
+
+
+
+
+
+
+/*var currentItems = [
+	{ id: 1, desc: 'Eric' },
+	{ id: 2, desc: 'Cheryl' },
+	{ id: 3, desc: 'Avery' }
+]*/
+
+
 
 
 
 app.get('/', function (req,res) {
-	// load data from db here
+	// load data from db to view here
 	res.render('index',{
-		title: 'My App',
+		title: jsonContent.databaseName,
 		items: currentItems
 	});
 });
