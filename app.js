3a487e81110656bb7ae3fcc36212d7a5d7cc1aaa
Eric Phung <esphung@iupui.edu>
2016-03-07 19:29:51 +0000

    Added write resident to db function

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
+/*
+AUTHOR:             Eric Phung
+CREATED:            2016.03.03 (March 3, 2016)
+PURPOSE:            Controller for single page, Express Node Js web app.
+NOTES:
 
+* Add functionality for resident activity page for staff
+
+* Implement "userActivityData" jsn property
+*/
 
 
 
@@ -22,21 +31,14 @@ var fs = require('fs')
 const PORT = 8000;
 var app = express();
 
-//var contents = fs.readFileSync("people.json");// ???
-
-//var jsonContent = JSON.parse(contents);
-
-//var residentList = getPersonDatabase();
-
-//var currentPersonObject = new Person();// ???
 
 
 
 // for resident being signed in and out to be stored
-var selectedResident = new Resident();
+//var selectedResident = new Resident();
 
 // read database json file
-var databaseFilecontents = fs.readFileSync("residentDatabase.json");
+var databaseFilecontents = fs.readFileSync("./models/activeDatabase.json");
 
 // parse the json data
 var jsonContent = JSON.parse(databaseFilecontents);
@@ -48,12 +50,12 @@ var residentList = getDatabaseResidents(jsonContent);
 
 // list of reasons for not being home
 var currentReasonsList = [
-	"Food",
-	"Church",
-	"Court",
-	"Work",
-	"Family",
-	"Holiday"
+    "Food",
+    "Church",
+    "Court",
+    "Work",
+    "Family",
+    "Holiday"
 ];
 
 
@@ -63,9 +65,14 @@ var currentReasonsList = [
 
 
 
+
+
+
+
+
 // configure app ===========================================
-app.set('view engine','ejs');
-app.set('views',path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 
 
@@ -81,10 +88,10 @@ app.set('views',path.join(__dirname, 'views'));
 
 
 app.use(bodyParser.urlencoded({
-	extended: true
+    extended: true
 }));
 
-app.use(express.static(path.join(__dirname,'bower_components')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 
 
 
@@ -98,27 +105,27 @@ app.use(express.static(path.join(__dirname,'bower_components')));
 
 
 // when client get requests the root page
-app.get('/', function (req,res) {
+app.get('/', function(req, res) {
 
-	var currentItemsList = getAllPresentPersonObjects(residentList)
-	currentItemsList = getAllFullNamesFromObjectList(currentItemsList);
+    var currentItemsList = getAllPresentPersonObjects(residentList)
+    currentItemsList = getAllFullNamesFromObjectList(currentItemsList);
 
 
-	// use this to see who gets selected
-	// load data from db to view here
-	res.render('index',{
-/*		isAuthenticated: req.isAuthenticated(),
-		user: req.user,*/
-		presentTableTitle: "Currently Home",
-		notPresentTableTitle: "Signed Out",
-		title: jsonContent.databaseName,
-		presentResidentsList: getAllPresentPersonObjects(residentList),
-		notPresentResidentsList: getAllNotPresentPersonObjects(residentList),
-		items: currentItemsList,
-		reasons: currentReasonsList
-	});
-	console.log('\nPage was refreshed..')
-	console.log('\nCurrent Residents at Home: ',currentItemsList)
+    // use this to see who gets selected
+    // load data from db to view here
+    res.render('index', {
+        /*		isAuthenticated: req.isAuthenticated(),
+        		user: req.user,*/
+        presentTableTitle: "Currently Home",
+        notPresentTableTitle: "Currently Signed Out",
+        title: jsonContent.databaseName,
+        presentResidentsList: getAllPresentPersonObjects(residentList),
+        notPresentResidentsList: getAllNotPresentPersonObjects(residentList),
+        items: currentItemsList,
+        reasons: currentReasonsList
+    });
+    console.log('\nPage was refreshed..')
+    console.log('\nCurrent Residents at Home: ', currentItemsList)
 });
 
 
@@ -133,69 +140,71 @@ app.get('/', function (req,res) {
 
 
 
-app.post('/add', function (req,res) {
-
-	var newItem = req.body.newItem;// this is an input's name attribute
-	var newReason = req.body.newReason;
-	console.log(newReason)
-
-
-	// get the current time as a human readable string
-	var d = new Date();
-	//newDate.setTime(unixtime*1000);
-	var timestamp = 1301090400
-	var newDate = new Date(timestamp*1000 + d.getTimezoneOffset() * 60000)
-	dateString = newDate.toUTCString();
-	console.log(dateString)
-
-
-	newReason = stripUserInputString(newReason, '/');
-	newItem = stripUserInputString(newItem, '/');
+app.post('/add', function(req, res) {
 
 
 
 
-	for (var i = residentList.length - 1; i >= 0; i--) {
-		if (newItem == residentList[i].id_num) {
+
+    var newItem = req.body.newItem; // this is an input's name attribute
+    var newReason = req.body.newReason;
+    console.log(newReason)
 
 
-			// check if user is signing IN
-			if (residentList[i].isPresent == false) {
-
-				residentList[i].toggleIsPresent();
-				residentList[i].timeStamp = dateString;
-				residentList[i].reasonNotPresent = null;
-				selectedResident = residentList[i];
-				res.redirect('/');
-				return;
-			} else{
-				// user is signing OUT
-
-				// check is they have a valid reason string to leave
-				if (newReason.length <= 0) {
-					res.redirect('/');
-					return;
-				} else {
-					residentList[i].toggleIsPresent();
-					residentList[i].timeStamp = dateString;
-					residentList[i].reasonNotPresent = newReason;
-					selectedResident = residentList[i];
-					res.redirect('/');
-					return;
-				}
+    var currentTimeString = timeNow();
 
 
-			}
+    newReason = stripUserInputString(newReason, '/');
+    newItem = stripUserInputString(newItem, '/');
 
 
 
 
-		};
-	};
+    for (var i = residentList.length - 1; i >= 0; i--) {
+        if (newItem == residentList[i].id_num) {
+
+
+            // check if user is signing IN
+            if (residentList[i].isPresent == false) {
+
+                residentList[i].toggleIsPresent();
+                residentList[i].timeStamp = currentTimeString;
+                residentList[i].reasonNotPresent = null;
+                res.redirect('/');
+                return;
+            } else {
+                // user is signing OUT
+
+                // check is they have a valid reason string to leave
+                if (newReason.length <= 0) {
+                    res.redirect('/');
+                    return;
+                } else {
+                    residentList[i].toggleIsPresent();
+                    residentList[i].timeStamp = currentTimeString;
+                    residentList[i].reasonNotPresent = newReason;
+
+
+                    //writeNewResidentObjectToDatabase(residentList[i])
 
 
 
-	res.redirect('/');
+                    res.redirect('/');
+                    return;
+                }
+
+
+            }
+
+
+
+
+        };
+    };
+
+
+
+    res.redirect('/');
 
 });
 
@@ -203,6 +212,24 @@ app.post('/add', function (req,res) {
 
 
 
+
+
+function writeNewResidentObjectToDatabase(new_resident){
+    // WRITE:
+    var myWriteFileValuePath = './models/activeDatabase.json'
+    //var jsonStr = '{"theTeam":[{"teamId":"1","status":"pending"},{"teamId":"2","status":"member"},{"teamId":"3","status":"member"}]}';
+
+    //var obj = JSON.parse(jsonStr);
+    //obj['theTeam'].push({"teamId":"4","status":"pending"});
+    jsonContent['residents'].push(new_resident)
+    //var jsonStr = JSON.stringify(jsonContent);
+    //console.log(jsonStr)
+
+    jsonfile.writeFileSync(myWriteFileValuePath, jsonContent)
+    //fd.writeFileSync(jsonStr)
+    // "{"theTeam":[{"teamId":"1","status":"pending"},{"teamId":"2","status":"member"},{"teamId":"3","status":"member"},{"teamId":"4","status":"pending"}]}"
+
+}
 
 
 
@@ -211,9 +238,9 @@ app.post('/add', function (req,res) {
 
 
 // tell the server to begin listening
-app.listen(PORT,function () {
-	// tell our server to start listening
-	console.log('ready on port ' + PORT)
+app.listen(PORT, function() {
+    // tell our server to start listening
+    console.log('ready on port ' + PORT)
 });
 
 
@@ -238,21 +265,24 @@ app.listen(PORT,function () {
 
 
 
-function getDatabaseResidents (content) {
+
+
+
+function getDatabaseResidents(content) {
 
     // create a virtual db
     var list = [];
     console.log("\n * Creating Database * \n");
 
     // construct db resident objects from json
-    content.residents.forEach(function (item) {
+    content.residents.forEach(function(item) {
         list.push(new Resident(
-        	item.id_num,
-        	item.firstName,
-        	item.lastName,
-        	item.isPresent,
-        	this.timeStamp,
-        	item.reasonNotPresent
+            item.id_num,
+            item.firstName,
+            item.lastName,
+            item.isPresent,
+            this.timeStamp,
+            item.reasonNotPresent
         ));
     })
 
@@ -260,7 +290,7 @@ function getDatabaseResidents (content) {
 
     return list;
 
-}// end set up db def
+} // end set up db resident objects function def
 
 
 
@@ -268,38 +298,38 @@ function getDatabaseResidents (content) {
 
 
 
-function getAllFullNamesFromObjectList (argument) {
-	// get full names from object list argument
-	var list = [];
-	argument.forEach( function(item) {
-		if (item.isPresent == true) {
-			list.push(item.firstName + " " + item.lastName);
-		}
-	});
-	return list;
+function getAllFullNamesFromObjectList(argument) {
+    // get full names from object list argument
+    var list = [];
+    argument.forEach(function(item) {
+        if (item.isPresent == true) {
+            list.push(item.firstName + " " + item.lastName);
+        }
+    });
+    return list;
 }
 
 
-function getAllPresentPersonObjects (argument) {
-	// takes list of all person objs and returns list of person objects that are present
-	var list = [];
-	argument.forEach( function(item) {
-		if (item.isPresent == true) {
-			list.push(item);
-		}
-	});
-	return list;
+function getAllPresentPersonObjects(argument) {
+    // takes list of all person objs and returns list of person objects that are present
+    var list = [];
+    argument.forEach(function(item) {
+        if (item.isPresent == true) {
+            list.push(item);
+        }
+    });
+    return list;
 }
 
-function getAllNotPresentPersonObjects (argument) {
-	// takes list of all person objs and returns list of person objects that are present
-	var list = [];
-	argument.forEach( function(item) {
-		if (item.isPresent != true) {
-			list.push(item);
-		}
-	});
-	return list;
+function getAllNotPresentPersonObjects(argument) {
+    // takes list of all person objs and returns list of person objects that are present
+    var list = [];
+    argument.forEach(function(item) {
+        if (item.isPresent != true) {
+            list.push(item);
+        }
+    });
+    return list;
 }
 
 
@@ -307,74 +337,85 @@ function getAllNotPresentPersonObjects (argument) {
 
 
 function stripUserInputString(str, delimiter) {
-	// delimiter is what to escape
-	//  discuss at: http://phpjs.org/functions/preg_quote/
-	// original by: booeyOH
-	// improved by: Ates Goral (http://magnetiq.com)
-	// improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-	// improved by: Brett Zamir (http://brett-zamir.me)
-	// bugfixed by: Onno Marsman
-	//   example 1: preg_quote("$40");
-	//   returns 1: '\\$40'
-	//   example 2: preg_quote("*RRRING* Hello?");
-	//   returns 2: '\\*RRRING\\* Hello\\?'
-	//   example 3: preg_quote("\\.+*?[^]$(){}=!<>|:");
-	//   returns 3: '\\\\\\.\\+\\*\\?\\[\\^\\]\\$\\(\\)\\{\\}\\=\\!\\<\\>\\|\\:'
+    // delimiter is what to escape
+    //  discuss at: http://phpjs.org/functions/preg_quote/
+    // original by: booeyOH
+    // improved by: Ates Goral (http://magnetiq.com)
+    // improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // improved by: Brett Zamir (http://brett-zamir.me)
+    // bugfixed by: Onno Marsman
+    //   example 1: preg_quote("$40");
+    //   returns 1: '\\$40'
+    //   example 2: preg_quote("*RRRING* Hello?");
+    //   returns 2: '\\*RRRING\\* Hello\\?'
+    //   example 3: preg_quote("\\.+*?[^]$(){}=!<>|:");
+    //   returns 3: '\\\\\\.\\+\\*\\?\\[\\^\\]\\$\\(\\)\\{\\}\\=\\!\\<\\>\\|\\:'
 
-	return String(str)
-		.replace(new RegExp('[.\\\\+*?\\[\\^\\]$(){}=!<>|:\\' + (delimiter || '') + '-]', 'g'), '\\$&');
-}// end string validation
-
-
+    return String(str)
+        .replace(new RegExp('[.\\\\+*?\\[\\^\\]$(){}=!<>|:\\' + (delimiter || '') + '-]', 'g'), '\\$&');
+} // end string validation
 
 
+function timeNow() {
+    var d = new Date(),
+        h = (d.getHours() < 10 ? '0' : '') + d.getHours(),
+        m = (d.getMinutes() < 10 ? '0' : '') + d.getMinutes();
+    h = h - 5;
+    if (h <= 12) {
+        return h + ':' + m + " a.m.";
+    } else {
+        return h + ':' + m + " a.m.";
+    }
+
+} // end get time now function def
 
 
 
-// constructor null
-function Resident(){
+
+/*// constructor null
+function Resident() {
     this.id_num = null;
     this.firstName = null;
     this.lastName = null;
     this.isPresent = true;
     this.timeStamp = null;
     this.reasonNotPresent = null;
-}// end null def
+} // end null*/
 
 
 // constructer overloaded
-function Resident(id_num,f_name,l_name,present_bool, reason){
+function Resident(id_num, f_name, l_name, present_bool, reason) {
     this.id_num = id_num;
     this.firstName = f_name;
     this.lastName = l_name;
     this.isPresent = present_bool;
     this.timeStamp = null;
-    if (reason != null){
+    if (reason != null) {
         this.reasonNotPresent = reason;
     } else {
         this.reasonNotPresent = null;
-        }
-}// end overload def
+    }
+} // end overload def
 
 
 
-Resident.prototype.getFullName = function(){
+Resident.prototype.getFullName = function() {
     return this.firstName + " " + this.lastName;
 }
 
-Resident.prototype.toggleIsPresent = function(){
-	this.isPresent = !this.isPresent;
-	if (this.isPresent == true) {
-		console.log(this.getFullName() + " has signed in!")
-	} else {
-		console.log(this.getFullName() + " has signed out!")
-	}
+Resident.prototype.toggleIsPresent = function() {
+    this.isPresent = !this.isPresent;
+    if (this.isPresent == true) {
+        console.log(this.getFullName() + " has signed in!")
+    } else {
+        console.log(this.getFullName() + " has signed out!")
+    }
 }
 
 
 
-Resident.prototype.setReasonNotPresent = function(str){
-    if (str.length > 0){
+Resident.prototype.setReasonNotPresent = function(str) {
+    if (str.length > 0) {
         this.reasonNotPresent = str;
         return true;
     } else {
@@ -384,25 +425,32 @@ Resident.prototype.setReasonNotPresent = function(str){
 }
 
 
-Resident.prototype.getReasonNotPresent = function(){
+Resident.prototype.getReasonNotPresent = function() {
     return this.reasonNotPresent;
 }
 
 
 
 
-Resident.prototype.setIsPresent = function(bool_val){
+Resident.prototype.setIsPresent = function(bool_val) {
     this.isPresent = bool_val;
 }
 
 
 
-// methods
-Resident.prototype.getIsPresent = function(){
-    if (this.isPresent == true){
+
+Resident.prototype.getIsPresent = function() {
+    if (this.isPresent == true) {
         alert("Present")
         return true;
     }
     return false;
 }
+
+
+
+
+
+
+
 
